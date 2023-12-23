cb12928d0113aceb09a88173c67e3965c89fe620
Brandon Matran <brandon.matran@gmail.com>
2023-12-23 00:15:56 -0800

    shopify section added

--- js/scripts.js
+++ js/scripts.js
@@ -33,59 +33,6 @@ function hideContainer() {
   helpListContainer.style.opacity = 0;
 }
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   let slideIndex = 1;
-//   let slides = document.getElementsByClassName("mySlides");
-//   console.log(slides[1].clientWidth);
-
-//   let counter = 1;
-//   const size = slides[0].clientWidth;
-
-//   function plusSlides(n) {
-//     showSlides((slideIndex += n));
-//   }
-
-//   function showSlides(n) {
-//     let i;
-
-//     if (n > slides.length) {
-//       slideIndex = 1;
-//       counter++;
-//     }
-//     if (n < 1) {
-//       slideIndex = slides.length;
-//       counter--;
-//     }
-//     if (counter % 2 === 0) {
-//       updateImageSources(
-//         "./resources/products/img1.png",
-//         "./resources/products/img2.png",
-//         "./resources/products/img3.png"
-//       );
-//     } else {
-//       updateImageSources(
-//         "./resources/products/img4.png",
-//         "./resources/products/img5.png",
-//         "./resources/products/img6.png"
-//       );
-//     }
-//         // Apply transition and transform for the sliding effect
-//         for (let i = 0; i < slides.length; i++) {
-//           slides[i].style.transition = "transform 0.4s ease-in-out";
-
-//         }
-//   }
-
-//   function updateImageSources(source1, source2, source3) {
-//     document.getElementById("image1").src = source1;
-//     document.getElementById("image2").src = source2;
-//     document.getElementById("image3").src = source3;
-//   }
-
-//   showSlides(slideIndex);
-//   window.plusSlides = plusSlides;
-// });
-
 document.addEventListener("DOMContentLoaded", function() {
 const carouselSlide = document.querySelector('.slideshow-slide');
 const carouselImages = document.querySelectorAll('.slideshow-slide img');
@@ -101,23 +48,24 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 
 nextBtn.addEventListener( 'click', ()=> {
-  if (counter >= carouselImages.length -1) return;
+  if (counter >= carouselImages.length) return;
   carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-  counter++;
+  counter+=3;
   carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 })
 
 prevBtn.addEventListener( 'click', ()=> {
   if (counter <= 0) return;
   carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-  counter--;
+  counter-=3;
   carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 })
 
 carouselSlide.addEventListener('transitionend', ()=> {
   if (carouselImages[counter].id ==='lastClone') {
+    console.log(carouselImages[counter].id)
     carouselSlide.style.transition = 'none';
-    counter = carouselImages.length - 2
+    counter = carouselImages.length
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
   }
 })
