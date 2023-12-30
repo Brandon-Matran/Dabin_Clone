a511a87d762cf75e93523694c8b9d228e7264efb
Brandon Matran <brandon.matran@gmail.com>
2023-12-29 21:57:00 -0800

    Added scripts to all pages

--- js/scripts.js
+++ js/scripts.js
@@ -133,38 +133,38 @@ carouselSlide.addEventListener('transitionend', () => {
 })
 
 
-/* Burger icon */
+// /* Burger icon */
 
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-  var iconMenu = document.getElementById('icon-menu')
+//   var iconMenu = document.getElementById('icon-menu')
 
-  console.log(iconMenu)
-  var hiddenMenu = document.getElementById('hidden-menu')
+//   console.log(iconMenu)
+//   var hiddenMenu = document.getElementById('hidden-menu')
 
-  console.log(hiddenMenu)
-
-
-
-  iconMenu.addEventListener('click', () => {
-    console.log("FIRED")
-    hiddenMenu.classList.toggle('active')
-    //hiddenMenu.style.display = (hiddenMenu.style.display ==='block') ? 'none' : 'block';
-
-})});
+//   console.log(hiddenMenu)
 
 
-/* Close hidden menu */
 
-document.addEventListener('DOMContentLoaded', () => {
-  var menuClose = document.getElementById('hidden-menu-close');
-  var hiddenMenu = document.getElementById('hidden-menu')
+//   iconMenu.addEventListener('click', () => {
+//     console.log("FIRED")
+//     hiddenMenu.classList.toggle('active')
+//     //hiddenMenu.style.display = (hiddenMenu.style.display ==='block') ? 'none' : 'block';
 
-  console.log(menuClose)
-  menuClose.addEventListener('click', () => {
-  //  hiddenMenu.style.display = 'none';
-    hiddenMenu.classList.remove('active')
+// })});
 
-  })
-});
+
+// /* Close hidden menu */
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   var menuClose = document.getElementById('hidden-menu-close');
+//   var hiddenMenu = document.getElementById('hidden-menu')
+
+//   console.log(menuClose)
+//   menuClose.addEventListener('click', () => {
+//   //  hiddenMenu.style.display = 'none';
+//     hiddenMenu.classList.remove('active')
+
+//   })
+// });
