55462ea19e869e2df7a317cd00f260670360cd53
Brandon Matran <brandon.matran@gmail.com>
2023-12-29 21:05:13 -0800

    Hidden menu added

--- js/scripts.js
+++ js/scripts.js
@@ -104,7 +104,7 @@ nextBtn.addEventListener( 'click', ()=> {
 })
 
 prevBtn.addEventListener( 'click', ()=> {
-  
+
   if (counter <= 0) return;
   if (window.innerWidth >= 915) {
   carouselSlide.style.transition = 'transform 0.4s ease-in-out';
@@ -121,7 +121,7 @@ prevBtn.addEventListener( 'click', ()=> {
 carouselSlide.addEventListener('transitionend', () => {
   if (carouselImages[counter].id === 'lastClone') {
       carouselSlide.style.transition = 'none';
-      counter = carouselImages.length - 2; 
+      counter = carouselImages.length - 2;
       carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
   } else if (carouselImages[counter].id === 'firstClone') {
       carouselSlide.style.transition = 'none';
@@ -131,3 +131,37 @@ carouselSlide.addEventListener('transitionend', () => {
 });
 
 })
+
+
+/* Burger icon */
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  var iconMenu = document.getElementById('icon-menu')
+
+  console.log(iconMenu)
+  var hiddenMenu = document.getElementById('hidden-menu')
+
+  console.log(hiddenMenu)
+
+
+
+  iconMenu.addEventListener('click', () => {
+    console.log("FIRED")
+    hiddenMenu.style.display = (hiddenMenu.style.display ==='block') ? 'none' : 'block';
+
+})});
+
+
+/* Close hidden menu */
+
+document.addEventListener('DOMContentLoaded', () => {
+  var menuClose = document.getElementById('hidden-menu-close');
+  var hiddenMenu = document.getElementById('hidden-menu')
+
+  console.log(menuClose)
+  menuClose.addEventListener('click', () => {
+    hiddenMenu.style.display = 'none';
+  })
+});
